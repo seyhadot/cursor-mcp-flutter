402d6cfcc12cbde7b09eaa11f35250f1c5b20ee5
seyhadot <54014691+seyhadot@users.noreply.github.com>
2025-03-06 10:07:34 +0700

    Add theme provider

---
+++ lib/providers/theme_provider.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences.dart';
+
+class ThemeProvider with ChangeNotifier {
+  ThemeMode _themeMode = ThemeMode.light;
+  ThemeMode get themeMode => _themeMode;
+
+  ThemeProvider() {
+    _loadTheme();
+  }
+
+  void _loadTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    final isDark = prefs.getBool('isDark') ?? false;
+    _themeMode = isDark ? ThemeMode.dark : ThemeMode.light;
+    notifyListeners();
+  }
+
+  void toggleTheme() async {
+    _themeMode = _themeMode == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('isDark', _themeMode == ThemeMode.dark);
+    notifyListeners();
+  }
+}
